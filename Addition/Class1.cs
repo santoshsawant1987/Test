a2f62d705c0c0f6c7fb0f547749472d2ce50b54e
Santosh Sawant <santoshsawant637@gmail.com>
2019-02-26 19:27:07 +0530

    Add project files.

---
+++ Addition/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition
+{
+    public class Class1
+    {
+        int s;
+        public int addition(int x,int y)
+        {
+            return x + y;
+        }
+    }
+}
